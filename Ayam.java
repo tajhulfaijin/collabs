2b5c163ba35fe72fc47128281be27b5088ff4b99
Dede Hendri <dhendri575@gmail.com>
2017-09-23 12:13:32 +0700

    file ini baru ditambahkan

---
+++ Ayam.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Ayam extends Unggas implements Naga{
+
+
+
+
+    public static void main(String[]args){
+        Unggas aym = new Unggas();
+      System.out.println(aym.jalan);
+     Scanner scan = new Scanner(System.in);
+      int jml = scan.nextInt();
+      { int i=1;
+        do {
+            System.out.println(i+smbr);
+            i++;
+        }while(i<=jml);
+    
+    }
+}
+
+    @Override
+    public void smbr(String smbr) {
+        System.out.println("Sembur");
+
+    }
+    }
